e4feaeed6dd170fd3cc9c6a0682b855d97ca12d2
OpenDevin <opendevin@opendevin.ai>
2024-06-06 03:15:21 +0000

    Add Cloudflare Workers configuration

---
+++ workers-site/index.js
@@ -0,0 +1,11 @@
+
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
+
+async function handleRequest(request) {
+  return new Response('Hello, world!', {
+    headers: { 'content-type': 'text/plain' },
+  })
+}
+
